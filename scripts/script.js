38f630dd4f08446dda1ab5183f9ecb7211453aff
Moneeb-Ahmad <moneebahmad32@gmail.com>
2022-06-24 14:13:53 -0400

    Operate function added

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,14 @@
 let x = 5;
 let y = 2;
-console.log(add(x,y));
-console.log(subtract(x,y));
-console.log(multiply(x,y));
-console.log(divide(x,y));
+const PLUS = "plus";
+const SUB = "sub";
+const MUL = "mul";
+const DIVIDE = "divide";
+
+console.log(operate(PLUS, x, y));
+console.log(operate(SUB, x, y));
+console.log(operate(MUL, x, y));
+console.log(operate(DIVIDE, x, y));
 
 function add(n1, n2) {
   return n1 + n2;
@@ -13,10 +18,29 @@ function subtract(n1, n2) {
   return n1 - n2;
 }
 
-function multiply(n1,n2) {
+function multiply(n1, n2) {
   return n1 * n2;
 }
 
 function divide(n1, n2) {
-  return n1 / n2;
+  return n2 === 0 ? "OOPS" : n1 / n2;
+}
+
+function operate(op, n1, n2) {
+  switch (op) {
+    case PLUS:
+      return add(n1, n2);
+      break;
+    case SUB:
+      return subtract(n1, n2);
+      break;
+    case MUL:
+      return multiply(n1, n2);
+    case DIVIDE:
+      return divide(n1, n2);
+    default:
+      return "OOPS";
+      break;
+  }
+  return "OOPS";
 }
