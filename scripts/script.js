a5940fec549b54ebb692f3cfef731f1ae5a64efc
Moneeb-Ahmad <moneebahmad32@gmail.com>
2022-06-24 19:53:13 -0400

    Display logic done

--- scripts/script.js
+++ scripts/script.js
@@ -4,11 +4,17 @@ const PLUS = "plus";
 const SUB = "sub";
 const MUL = "mul";
 const DIVIDE = "divide";
+const buttons = document.querySelectorAll('button');
+const display = document.querySelector('.display');
+const operations=['*','+','-','/','='];
 
 console.log(operate(PLUS, x, y));
 console.log(operate(SUB, x, y));
 console.log(operate(MUL, x, y));
 console.log(operate(DIVIDE, x, y));
+
+let dispVal = '';
+let dispCounter = 0;
 
 function add(n1, n2) {
   return n1 + n2;
@@ -44,3 +50,39 @@ function operate(op, n1, n2) {
   }
   return "OOPS";
 }
+
+function updateDisplay(input) {
+  if(dispCounter >= 12) {
+    if(input == "Clear") {
+      dispVal = "";
+      display.textContent = '';
+      dispCounter = 0;
+    }
+    return;
+  }
+  switch(input) {
+    case "Clear":
+      dispVal = "";
+      dispCounter = 0;
+      break;
+    default:
+      break;
+  }
+  display.textContent = dispVal;
+}
+
+buttons.forEach((button) => {
+  button.addEventListener('click', () => {
+    let input = button.textContent.trim();
+    let operation = '';
+    if(!operations.includes(input)) {
+      dispVal += input;
+      dispCounter++;
+    }
+    else {
+      operation = '';
+    }
+    updateDisplay(input);
+    console.log(button.textContent.trim());
+  });
+});
