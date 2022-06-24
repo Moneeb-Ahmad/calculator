9d5e1eb043722524417f79f45547cfdfe4a064eb
Moneeb-Ahmad <moneebahmad32@gmail.com>
2022-06-24 14:07:07 -0400

    Basic operations done

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,22 @@
+let x = 5;
+let y = 2;
+console.log(add(x,y));
+console.log(subtract(x,y));
+console.log(multiply(x,y));
+console.log(divide(x,y));
 
+function add(n1, n2) {
+  return n1 + n2;
+}
+
+function subtract(n1, n2) {
+  return n1 - n2;
+}
+
+function multiply(n1,n2) {
+  return n1 * n2;
+}
+
+function divide(n1, n2) {
+  return n1 / n2;
+}
