087b8b9366d5a315b3440bf95249e6df3599ee0f
Moneeb-Ahmad <moneebahmad32@gmail.com>
2022-06-25 20:25:30 -0400

    Fixed multiple operations in a row issue

--- scripts/script.js
+++ scripts/script.js
@@ -10,6 +10,7 @@ let operFlip = false;
 let opDone = false;
 let op1 = true;
 let lastOperation = '';
+let opMode = false;
 let no1 = 0;
 let no2 = 0;
 let eqFlip = false;
@@ -34,6 +35,21 @@ function divide(n1, n2) {
   return ret;
 }
 
+function reset() {
+  dispVal = "";
+  display.textContent = '';
+  dispCounter = 0;
+  op1 = true;
+  operFlip = false;
+  lastOpChange = false;
+  dontCall = false;
+  eqFlip = false;
+  no1 = 0;
+  no2 = 0;
+  lastOperation = '';
+  opMode = true;
+}
+
 function operate(op, n1, n2) {
   switch (op) {
     case PLUS:
@@ -56,17 +72,7 @@ function operate(op, n1, n2) {
 function updateDisplay(input, operation) {
   if (dispCounter >= 11 && !operations.includes(operation)) {
     if (input == "Clear") {
-      dispVal = "";
-      display.textContent = '';
-      dispCounter = 0;
-      op1 = true;
-      operFlip = false;
-      lastOpChange = false;
-      dontCall = false;
-      eqFlip = false;
-      no1 = 0;
-      no2 = 0;
-      lastOperation = '';
+      reset();
     }
     return;
   }
@@ -83,6 +89,7 @@ function updateDisplay(input, operation) {
         opDone = true;
         op1 = false;
         lastOperation = operation;
+        opMode = true;
       } else {
         lastOperation = operation;
         if (lastOperation === EQ) {
@@ -91,13 +98,13 @@ function updateDisplay(input, operation) {
           no1 = Number(dispVal);
           dispVal = '';
           dispCounter = 0;
+          opMode = true;
         }
       }
     } else {
       if (!eqFlip) {
         if (operation === EQ) {
           eqFlip = true;
-
         }
         no2 = Number(dispVal);
         dispVal = operate(lastOperation, no1, no2);
@@ -105,6 +112,7 @@ function updateDisplay(input, operation) {
         dispVal = dispVal.toString();
         opDone = true;
         lastOperation = operation;
+        opMode = true;
       } else {
         eqFlip = false;
         lastOperation = operation
@@ -113,21 +121,8 @@ function updateDisplay(input, operation) {
 
 
   } else {
-    switch (input) {
-      case "Clear":
-        dispVal = "";
-        display.textContent = '';
-        dispCounter = 0;
-        op1 = true;
-        operFlip = false;
-        no1 = 0;
-        no2 = 0;
-        eqFlip = false;
-        lastOpChange = false;
-        lastOperation = '';
-        break;
-      default:
-        break;
+    if (input === "Clear") {
+      reset();
     }
   }
   display.textContent = dispVal;
@@ -145,27 +140,20 @@ buttons.forEach((button) => {
     let operation = '';
     if (!operations.includes(input)) {
       if (eqFlip) {
-        dispVal = "";
-        display.textContent = '';
-        dispCounter = 0;
-        op1 = true;
-        operFlip = false;
-        lastOpChange = false;
-        dontCall = false;
-        no1 = 0;
-        eqFlip = false;
-        no2 = 0;
-        lastOperation = '';
+        reset();
       }
       dispVal += input;
       dispCounter++;
       operation = '';
+      opMode = false;
     } else {
       if (eqFlip) {
         if (input === EQ) {
           dontCall = true;
         }
-
+      }
+      if (opMode) {
+        dontCall = true
       }
       operation = input;
       operFlip = operFlip ? false : true;
